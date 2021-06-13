8b4220d9dd32c0448f6c708b235dad5dd61e55d5
Sergey Kostanets <sergeykostanets@gmail.com>
2021-06-13 12:09:01 +0300

    load files

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    $('.info-tabs__item').on('click', function(e){
+        e.preventDefault();
+
+        $('.info-tabs__item').removeClass('info-tabs__item--active');
+        $(this).addClass('info-tabs__item--active');
+
+        $('.info-tabs__content').removeClass('info-tabs__content--active');
+        $($(this).attr('href')).addClass('info-tabs__content--active');
+    });
+
+    $('.info-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+});
